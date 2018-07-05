72b4051618565458ff1fab8590a9901e783040bc
MayaCherry1 <mrcherry@wisc.edu>
2018-07-05 10:15:55 -0500

    deleted unnecessary files

--- lib/end_game.rb
+++ lib/end_game.rb
@@ -1,5 +1,10 @@
 class EndGame
 
+	def initialize (num_guesses, secret)
+		if num_guesses  10
+			game_lose(secret)
+	end
+
 	def game_win (num_guesses, secret)
 		puts "Congratulations! The secret code was #{secret.join(" ")}"
 		puts "You won in #{num_guesses} guesses"
